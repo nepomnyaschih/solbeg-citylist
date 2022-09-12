31e9233305e893e5e97465181e3c87ea69a3f2da
nepomnyaschih <nepomnyaschih.s@gmail.com>
2022-09-12 22:07:29 +0400

    init

---
+++ back-java/src/main/java/com/solbeg/citylist/CitylistApplication.java
@@ -0,0 +1,13 @@
+package com.solbeg.citylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitylistApplication.class, args);
+	}
+
+}
---
+++ back-java/src/test/java/com/solbeg/citylist/CitylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbeg.citylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
